1d852e7985a2aa39b082ce91f90b78b547302d32
Corentin1998 <72392632+Corentin1998@users.noreply.github.com>
2020-11-02 16:04:58 +0100

    Pong 02.11.2020

---
+++ js/pong.js
@@ -0,0 +1,14 @@
+
+
+let largeur$("#balle").width();
+let gauche=parseInt($("#balle").css.("left"));
+let haut=parseInt($("#balle").css.("left"));
+alert(gauche);
+
+let i=0;
+setInterval(function(){
+    gauche-gauche
+    haut-haut
+    $("#balle").css("left",gauche);
+    $("#balle").css("top",haut);
+}, 10);
