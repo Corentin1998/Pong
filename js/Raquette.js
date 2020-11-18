2674c6841c782e1df107609e76c3c3fd0dcce368
Corentin1998 <corentin.hbb@gmail.com>
2020-11-18 15:29:28 +0100

    Changements du 18.11.2020

--- js/Raquette.js
+++ js/Raquette.js
@@ -1,28 +1,19 @@
-class Raquette1{
+class Raquette{
     constructor($html){
         this.$html=$html;
         this.haut=parseInt($("#raquette1").css("top"));
         this.hauteur=parseInt($("#raquette1").css("height"));
-        this.vitesse=1;
-        this.direction=1;
-    }
-    majHTML(){
-        this.$html.css("top",raquette1.haut);
-    }
-}
-
-class Raquette2{
-    constructor($html){
-        this.$html=$html;
         this.haut=parseInt($("#raquette2").css("top"));
         this.hauteur=parseInt($("#raquette2").css("height"));
         this.vitesse=1;
         this.direction=1;
     }
     majHTML(){
+        this.$html.css("top",raquette1.haut);
         this.$html.css("top",raquette2.haut);
+
     }
 }
 
-let raquette1 = new Raquette1($("#raquette1"));
-let raquette2 = new Raquette2($("#raquette2"));
+let raquette1 = new Raquette($("#raquette1"));
+let raquette2 = new Raquette($("#raquette2"));
