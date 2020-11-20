b14d104cc5471648f63fd527b4647ba2bf1d8ee4
Corentin1998 <72392632+Corentin1998@users.noreply.github.com>
2020-11-20 17:11:39 +0100

    Update Terrain.js

--- js/Terrain.js
+++ js/Terrain.js
@@ -1,7 +1,7 @@
 class Terrain{
     constructor($element){ // $ = élément de la bibliothèque (Jquery)
         this.$element;
-        this.largeur=$element.width(); // pour la largeur du terrain, se référer à "width" de "#terrain" (css)
-        this.hauteur=$element.height(); // pour la hauteur du terrain, se référer à "height" de "#terrain" (css)
+        this.largeur=$element.width();
+        this.hauteur=$element.height();
     }
 }
